0a1af9f32f3bbb7f6335eeb568e01eef5401da41
1DAW28 <1DAW28@INFORMATICA-128.DOMDPTOINFJMPP.INF>
2022-03-21 15:47:25 +0100

    v1.0
    Versión 1.

---
+++ src/main/java/practicapmd/PracticaPMD.java
@@ -0,0 +1,14 @@
+package practicapmd;
+public class PracticaPMD {
+    public static void main(String[] args) {
+        int altura=89;
+        if (altura > 89) {
+            System.out.println("Eres muy alto");
+        }
+        PracticaPMD ejemPMD = new PracticaPMD();
+        ejemPMD.metodoDobleReturn();
+    }
+    public boolean metodoDobleReturn() {
+        return false;
+    }
+}
